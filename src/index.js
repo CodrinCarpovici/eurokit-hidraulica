af345e984accfb342a7808bf82c525de72316f80
Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2022-01-04 13:28:29 +0000

    fixes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter baseUrl="/eurokit-hidraulica">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
